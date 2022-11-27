c7b3bfb8023e12a65a2332da4d74e410cafd2944
cxu <cxu@wesleyan.edu>
2022-11-27 16:58:05 -0500

    initialize

---
+++ summary check.R
@@ -0,0 +1,9 @@
+ped <- read.csv("ped_crashes.csv")
+library(qacBase)
+contents(ped)
+df_plot(ped)
+cor_plot(ped)
+
+ped <- subset(ped, Weather.Conditions..2016..!="Uncoded & errors")
+ped <- subset(ped, Person.Gender != "Uncoded & errors")
+ped <- subset(ped, Person.Age != "DOB invalid")
